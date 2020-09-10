8d7ae48770bfb1ac547da7f4b4558a815a7455c6
Temelius <teme989@gmail.com>
2020-09-10 21:53:35 +0300

    Initial project commit

---
+++ src/test/java/com/temelius/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temelius.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
