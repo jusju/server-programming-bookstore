c6f47f510866f765e94c60016dd7c38fe3a78bb4
Temelius <teme989@gmail.com>
2020-09-17 02:40:38 +0300

    Add few more books to test data

--- src/main/java/com/temelius/bookstore/BookstoreApplication.java
+++ src/main/java/com/temelius/bookstore/BookstoreApplication.java
@@ -25,7 +25,12 @@ public class BookstoreApplication {
 			repository.save(new Book("Ernest Hemingway", "A Farewell to Arms", "1232323-21", "1929"));
 			repository.save(new Book("George Orwell", "Animal Farm", "2212343-5", "1945"));
 			repository.save(new Book("J. K. Rowling", "Harry Potter and the Philosopher’s Stone", "951-31-1146-6", "1997"));
-			
+			repository.save(new Book("J. K. Rowling", "Harry Potter and the Chamber of Secrets", "951-31-1472-4", "1998"));
+			repository.save(new Book("J. K. Rowling", "Harry Potter and the Prisoner of Azkaban", "951-31-1737-5", "1999"));
+			repository.save(new Book("J. K. Rowling", "Harry Potter and the Goblet of Fire", "951-31-2038-4", "2000"));
+			repository.save(new Book("J. K. Rowling", "Harry Potter and the Order of the Phoenix", "951-31-2907-1", "2003"));
+			repository.save(new Book("J. K. Rowling", "Harry Potter and the Half-Blood Prince", "951-31-3507-1", "2005"));
+			repository.save(new Book("J. K. Rowling", "Harry Potter and the Deathly Hallows", "978-951-31-4004-5", "2007"));
 			
 			for (Book book : repository.findAll()) {
 				log.info(book.toString());
