5c6b7c87eaecacd100bb2fa6c4e28caddd41df93
Temelius <teme989@gmail.com>
2020-09-17 01:03:57 +0300

    Add BookstoreController

---
+++ src/main/java/com/temelius/bookstore/web/BookstoreController.java
@@ -0,0 +1,40 @@
+package com.temelius.bookstore.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.temelius.bookstore.domain.*;
+
+@Controller
+public class BookstoreController {
+	@Autowired
+	private BookstoreRepository repository;
+	
+	@RequestMapping(value = {"/", "/booklist"})
+	public String booklist(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
+	
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+}
