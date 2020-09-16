2bd4120619f28cfe34bb73e9a7711ff855d24a70
Temelius <teme989@gmail.com>
2020-09-17 00:46:58 +0300

    Add Repository interface

---
+++ src/main/java/com/temelius/bookstore/domain/BookstoreRepository.java
@@ -0,0 +1,9 @@
+package com.temelius.bookstore.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookstoreRepository extends CrudRepository<Book, Long>{
+	List<Book> findByTitle(String title);
+}
