2c7d4af068795d3bf4b2aa9c4b24197b3321c8d5
Temelius <teme989@gmail.com>
2020-10-07 18:27:31 +0300

    Add WebSecurityConfig class

---
+++ src/main/java/com/temelius/bookstore/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.temelius.bookstore;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/", "/booklist").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+	}
+}
